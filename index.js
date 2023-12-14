1bc63a055a0e4c4b1495dc700a304cae56990b2c
Devin Bielejec <devin.bielejec@gmail.com>
2023-12-14 17:33:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ checkConfig();
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const server = require('./server');
-const credentials = require("./config/ssl");
+// const credentials = require("./config/ssl");
 const https = require("https");
 const port = process.env.PORT || 443;
-
+const credentials = null;
 
 //Connect to MongoDB
 mongoose
